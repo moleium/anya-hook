d138e5d3d74a0861c85d0d8dc9ce75dd9f5dfdc8
Rexi <68111233+RexiRexii@users.noreply.github.com>
2022-09-26 00:23:45 +0300

    Update anya_hook.hpp
    disassembler for length shit

--- anya_hook.hpp
+++ anya_hook.hpp
@@ -8,6 +8,8 @@
 class anya_hook
 {
 public:
+	std::uintptr_t target_function; // detour
+
 	std::uint8_t* function_o; // old
 	std::uint8_t* function_t; // backup
 
@@ -16,8 +18,8 @@ public:
 public:
 	explicit anya_hook();
 
-	void detour(const std::uintptr_t to_hook, const std::uintptr_t to_replace);
-	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace);
+	void detour(const std::uintptr_t to_hook, const std::uintptr_t to_replace, const std::size_t length);
+	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace, std::int32_t instr_nops = 0);
 
 	void unhook(std::uintptr_t to_unhook);
 	void yield(const std::uintptr_t to_yield);
